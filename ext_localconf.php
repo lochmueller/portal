1222b36918c1d4116278362ebc78e7b709e47d6b
Tim Lochmüller <tim@fruit-lab.de>
2023-04-03 20:33:21 +0200

    Cleanups

---
+++ ext_localconf.php
@@ -0,0 +1,22 @@
+<?php
+
+# Backend configuration
+if ($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginLogo'] === '') {
+    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginLogo'] = 'EXT:portal/Resources/Public/Icons/ExtensionBig.svg';
+}
+
+if ($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['backendLogo'] === '') {
+    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['backendLogo'] = 'EXT:portal/Resources/Public/Icons/Extension.svg';
+}
+
+if ($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginBackgroundImage'] === '') {
+    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginBackgroundImage'] = 'EXT:portal/Resources/Public/Images/Login.jpg';
+}
+
+if ($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginFootnote'] === '') {
+    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginFootnote'] = 'From EXT:portal with ❤️';
+}
+
+if ($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginHighlightColor'] === '') {
+    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginHighlightColor'] = '#2a9d8f';
+}
